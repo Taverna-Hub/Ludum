1fd4aa2b3b6e472d6cfb23fa2b8c2b5bea3f21f5
Matheus <mhma@cesar.school>
2025-10-17 22:17:34 -0300

    Corrigido erro em JogoRepository

--- dominio-principal/src/main/java/org/ludum/catalogo/jogo/repositorios/JogoRepository.java
+++ dominio-principal/src/main/java/org/ludum/catalogo/jogo/repositorios/JogoRepository.java
@@ -3,7 +3,10 @@ package org.ludum.catalogo.jogo.repositorios;
 import org.ludum.catalogo.jogo.entidades.Jogo;
 import org.ludum.catalogo.jogo.entidades.JogoId;
 import org.ludum.catalogo.jogo.entidades.Slug;
+import org.ludum.catalogo.tag.entidades.TagId;
 import org.ludum.identidade.conta.entities.ContaId;
+
+import java.util.List;
 
 public interface JogoRepository {
 
@@ -14,4 +17,6 @@ public interface JogoRepository {
     Jogo obterPorSlug(Slug slug);
 
     boolean existeSlugParaDesenvolvedora(ContaId devId, Slug slug);
+
+    List<Jogo> obterJogosPorTag(TagId tagId);
 }
