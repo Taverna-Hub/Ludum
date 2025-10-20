4df1865be428b4b00b40a239b52445c0c3160786
Sophia-15 <deletrasopa@gmail.com>
2025-10-19 22:13:38 -0300

    Revert "Merge pull request #10 from Taverna-Hub/feat/dominio/review"
    This reverts commit 2b7d91b0e9a79c4a6d607b172b3e630abc79264f, reversing
    changes made to 5edbf989ed28ec135f70022711c7603feabe4f90.

--- dominio-principal/src/test/java/org/ludum/dominio/financeiro/ComprarJogoFuncionalidade.java
+++ dominio-principal/src/test/java/org/ludum/dominio/financeiro/ComprarJogoFuncionalidade.java
@@ -1,4 +1,4 @@
-package java.org.ludum.dominio.financeiro;
+package org.ludum.dominio.financeiro;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
