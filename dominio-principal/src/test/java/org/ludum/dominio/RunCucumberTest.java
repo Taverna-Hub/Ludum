dd04b7a9ed0c50d61c55c325b921c3a8cb590e06
Pandor4b <annagsilv4@gmail.com>
2025-10-21 21:52:56 -0300

    refactor: changed publication feature package to domain of catalogue

--- dominio-principal/src/test/java/org/ludum/dominio/RunCucumberTest.java
+++ dominio-principal/src/test/java/org/ludum/dominio/RunCucumberTest.java
@@ -15,12 +15,10 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ludum.dominio.financeiro," +
                                                           "org.ludum.dominio.comunidade," +
-                                                          "org.ludum.dominio.publicacao," +
                                                           "org.ludum.dominio.identidade," +
                                                           "org.ludum.dominio.catalogo")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/org/ludum/financeiro," +
                                                               "src/test/resources/org/ludum/comunidade," +
-                                                              "src/test/resources/org/ludum/publicacao," +
                                                               "src/test/resources/org/ludum/identidade," +
                                                               "src/test/resources/org/ludum/catalogo")
 public class RunCucumberTest {
