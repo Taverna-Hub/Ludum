e05e4bf9404496a60eebbc18975a44209aa0821d
Pandor4b <annagsilv4@gmail.com>
2025-10-21 21:59:46 -0300

    refactor: publication feature from publication package to domain catalogo package

--- dominio-principal/src/test/java/org/ludum/dominio/catalogo/PublicacaoJogoFuncionalidade.java
+++ dominio-principal/src/test/java/org/ludum/dominio/catalogo/PublicacaoJogoFuncionalidade.java
@@ -1,4 +1,4 @@
-package org.ludum.dominio.publicacao;
+package org.ludum.dominio.catalogo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
